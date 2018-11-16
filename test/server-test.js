c8b5824f959822f241c963f851f09d9763dfe2cd
tdog191 <twarther17@gmail.com>
2018-11-16 09:31:09 -0600

    Remove incomplete test files for now, rename 'test/test.js' to 'test/server-test.js', and polish 'test/server-test.js'.

--- test/server-test.js
+++ test/server-test.js
@@ -1,6 +1,6 @@
 var request = require("supertest");
 
-describe("loading express", function() {
+describe("express server", function() {
   var server;
 
   beforeEach(function() {
@@ -11,43 +11,43 @@ describe("loading express", function() {
     server.close();
   });
 
-  it("responds to /", function testSlash(done) {
+  it("responds on /", function (done) {
     request(server)
       .get("/")
       .expect(200, done);
   });
 
-  it("responds on /html/chat.html", function testSlash(done) {
+  it("responds on /html/chat.html", function (done) {
     request(server)
       .get("/html/chat.html")
       .expect(200, done);
   });
 
-  it("responds on /html/audio.html", function testSlash(done) {
+  it("responds on /html/audio.html", function (done) {
     request(server)
       .get("/html/audio.html")
       .expect(200, done);
   });
 
-  it("responds on /html/index.html", function testSlash(done) {
+  it("responds on /html/index.html", function (done) {
     request(server)
       .get("/html/index.html")
       .expect(200, done);
   });
 
-  it("responds on /html/users.html", function testSlash(done) {
+  it("responds on /html/users.html", function (done) {
     request(server)
       .get("/html/users.html")
       .expect(200, done);
   });
 
-  it("responds on /html/profile.html", function testSlash(done) {
+  it("responds on /html/profile.html", function (done) {
     request(server)
       .get("/html/profile.html")
       .expect(200, done);
   });
 
-  it("404 on /foo/bar", function testPath(done) {
+  it("returns 404 Not Found on /foo/bar", function testPath(done) {
     request(server)
       .get("/foo/bar")
       .expect(404, done);
