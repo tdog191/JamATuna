cfdf7093f450f609b632a13de5adae5ed01f2a67
tdog191 <twarther17@gmail.com>
2018-11-24 02:50:02 -0600

    Delete and recreate Firebase database, move configuration for new Firebase database to local secret configuration file, and add local secret configuration file to gitignore.

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -2,15 +2,8 @@
 
 function configureFirebase() {
   const firebase = require('firebase');
+  const firebaseConfig = require('./secret_config').firebaseConfig;
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyA4eLoMXqPmz-QchOzWaVT1LJ6R3LDOE4k",
-    authDomain: "jamatuna.firebaseapp.com",
-    databaseURL: "https://jamatuna.firebaseio.com",
-    projectId: "jamatuna",
-    storageBucket: "jamatuna.appspot.com",
-    messagingSenderId: "771905743407"
-  };
   firebase.initializeApp(firebaseConfig);
 }
 
