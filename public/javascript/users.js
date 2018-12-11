90ca1aad9ef1b39eb814eab4075979a984da9481
tdog191 <twarther17@gmail.com>
2018-12-11 00:44:42 -0600

    Move 'form_validation.js' and 'form_submission.js' to a new 'common' directory, update the login, signup, jam room creation, and jam room joining pages accordingly, and refactor search pages to reuse the same code.

--- public/javascript/users.js
+++ public/javascript/users.js
@@ -1,99 +1,16 @@
 /**
- * @fileoverview Fetches a list of all users from the server and displays it.
- *     Every odd-numbered user list entry is colored light-blue
- *     (has "list-group-item-info" class), and every even-numbered user list
- *     entry is colored green (has "list-group-item-success" class).
+ * @fileoverview Fetches a list of all users from the server and displays it
+ *     according to search criteria.
  */
-
-//TODO: Refactor users.js and view_jam_rooms.js to reuse duplicate code
 
 'use strict';
-
-const baseUrl = window.location.origin;
-let users = {};
-
-/**
- * Generates an HTML user list entry using jQuery.
- *
- * @param username The username inside the list entry
- * @param colorClassAttribute The class attribute that defines the color of the
- *     list entry
- */
-function appendUserListEntry(username, colorClassAttribute) {
-  const classAttribute = [
-    "list-group-item",
-    "list-group-item-action",
-    colorClassAttribute,
-  ];
-
-  const userListEntry =
-      $(`<a href="profile.html" class="${classAttribute.join(' ')}">`);
-
-  userListEntry.text(username);
-  userListEntry.on('click', function(event) {
-    sessionStorage.setItem('profile_username', username);
-  });
-
-  $('#users').append(userListEntry);
-}
-
-/**
- * Fetches all existing users, displays them, and saves them
- * so no further API calls will be necessary to filter them
- */
-function searchUsers() {
-  fetch(baseUrl + '/api/get_users')
-      .then(response => response.json())
-      .then(data => {
-        users = data;
-
-        let isBlueListEntry = true;
-
-        // Display every retrieved user, alternating between light-blue
-        // and green colors for each jam room list entry
-        Object.keys(data)
-            .map(user => {
-              if(isBlueListEntry) {
-                appendUserListEntry(user, 'list-group-item-info');
-              } else {
-                appendUserListEntry(user, 'list-group-item-success');
-              }
-
-              isBlueListEntry = !isBlueListEntry;
-            });
-      })
-      .catch(errorResponse => console.log(errorResponse));
-}
 
 /**
  * Fetches and displays all existing users upon loading the page
  */
 window.onload = function() {
-  searchUsers();
+  search('/api/get_users', false);
 };
-
-/**
- * Display only users with usernames that have the given prefix (ignoring case),
- * alternating between light-blue and green colors for each jam room list entry
- *
- * @param nameQuery The prefix to match usernames against (ignoring case)
- */
-function filterByName(nameQuery) {
-  let isBlueListEntry = true;
-
-  Object.keys(users)
-      .filter(username =>
-          new RegExp(`^${nameQuery}`).test(username.toLowerCase())
-      ).map(username => {
-        if(isBlueListEntry) {
-          appendUserListEntry(username, 'list-group-item-info');
-        } else {
-          appendUserListEntry(username, 'list-group-item-success');
-        }
-
-        isBlueListEntry = !isBlueListEntry;
-      });
-}
 
 /**
  * Defines the overall jquery functionality of the user list webpage:
@@ -109,6 +26,6 @@ $(function() {
 
     const searchQuery = $('#search_bar').val().toLowerCase();
 
-    filterByName(searchQuery);
+    filterByName(searchQuery, false);
   });
 });
--- public/javascript/view_jam_rooms.js
+++ public/javascript/view_jam_rooms.js
@@ -1,149 +1,16 @@
 /**
  * @fileoverview Fetches a list of all jam rooms from the server and displays
- *     it according to search criteria.  Every odd-numbered jam room list entry
- *     is colored light-blue (has "list-group-item-info" class), and every
- *     even-numbered jam room list entry is colored green
- *     (has "list-group-item-success" class).
+ *     it according to search criteria.
  */
 
 'use strict';
-
-const baseUrl = window.location.origin;
-let jamRooms = {};
-
-/**
- * Generates an HTML jam room list entry using jQuery.
- *
- * @param jamRoom The jam room inside the list entry
- * @param colorClassAttribute The class attribute that defines the color of the
- *     list entry
- */
-function appendJamRoomListEntry(jamRoom, colorClassAttribute) {
-  const classAttribute = [
-    "list-group-item",
-    "list-group-item-action",
-    colorClassAttribute,
-  ];
-
-  const jamRoomListEntry =
-      $(`<a href="join_jam_room.html" class="${classAttribute.join(' ')}">`);
-
-  jamRoomListEntry.text(jamRoom);
-  jamRoomListEntry.on('click', function(event) {
-    sessionStorage.setItem('jam_room', jamRoom);
-  });
-
-  $('#jam_rooms').append(jamRoomListEntry);
-}
-
-/**
- * Fetches all existing jam rooms, displays them, and saves them
- * so no further API calls will be necessary to filter them
- */
-function searchJamRooms() {
-  fetch(baseUrl + '/api/get_jam_rooms')
-      .then(response => response.json())
-      .then(data => {
-        jamRooms = data;
-
-        let isBlueListEntry = true;
-
-        // Display every retrieved jam room, alternating between light-blue
-        // and green colors for each jam room list entry
-        Object.keys(data)
-            .map(jamRoom => {
-              if(isBlueListEntry) {
-                appendJamRoomListEntry(jamRoom, 'list-group-item-info');
-              } else {
-                appendJamRoomListEntry(jamRoom, 'list-group-item-success');
-              }
-
-              isBlueListEntry = !isBlueListEntry;
-            });
-      })
-      .catch(errorResponse => console.log(errorResponse));
-}
 
 /**
  * Fetches and displays all existing jam rooms upon loading the page
  */
 window.onload = function() {
-  searchJamRooms();
+  search('/api/get_jam_rooms', true);
 };
-
-/**
- * Display only jam rooms with names that have the given prefix (ignoring case),
- * alternating between light-blue and green colors for each jam room list entry
- *
- * @param nameQuery The prefix to match jam room names against (ignoring case)
- */
-function filterByName(nameQuery) {
-  let isBlueListEntry = true;
-
-  Object.keys(jamRooms)
-      .filter(jamRoom =>
-          new RegExp(`^${nameQuery}`).test(jamRoom.toLowerCase())
-      ).map(jamRoom => {
-        if(isBlueListEntry) {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-info');
-        } else {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-success');
-        }
-
-        isBlueListEntry = !isBlueListEntry;
-      });
-}
-
-/**
- * Display only jam rooms with owners that have usernames that have the given
- * prefix (ignoring case), alternating between light-blue and green colors for
- * each jam room list entry
- *
- * @param ownerQuery The prefix to match jam room owner usernames against
- *     (ignoring case)
- */
-function filterByOwner(ownerQuery) {
-  let isBlueListEntry = true;
-
-  Object.entries(jamRooms)
-      .filter(([jamRoom, jamRoomData]) =>
-          new RegExp(`^${ownerQuery}`).test(jamRoomData.owner.toLowerCase())
-      ).map(entry => entry[0])
-      .map(jamRoom => {
-        if(isBlueListEntry) {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-info');
-        } else {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-success');
-        }
-
-        isBlueListEntry = !isBlueListEntry;
-      });
-}
-
-/**
- * Display only jam rooms with at least as many members as the given number
- * alternating between light-blue and green colors for each jam room list entry
- *
- * @param minimumMemberQuery The minimum number of members a displayed jam room
- *     must have
- */
-function filterByMinimumMembers(minimumMemberQuery) {
-  let isBlueListEntry = true;
-
-  Object.entries(jamRooms)
-      .filter(([jamRoom, jamRoomData]) =>
-          Object.keys(jamRoomData.members).length >= minimumMemberQuery
-      ).map(entry => entry[0])
-      .map(jamRoom => {
-        if(isBlueListEntry) {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-info');
-        } else {
-          appendJamRoomListEntry(jamRoom, 'list-group-item-success');
-        }
-
-        isBlueListEntry = !isBlueListEntry;
-      });
-}
 
 /**
  * Defines the overall jquery functionality of the jam room viewing webpage:
@@ -168,7 +35,7 @@ $(function() {
     } else if(isSearchByMinimumMembers) {
       filterByMinimumMembers(searchQuery);
     } else {
-      filterByName(searchQuery);
+      filterByName(searchQuery, true);
     }
   });
 });
